12acca9e217d8385a328c37907146725798d0ece
angusman <agusman@csu.fullerton.edu>
2016-11-12 15:30:57 -0800

    testing case study (in progress)

--- krdTesting.py
+++ krdTesting.py
@@ -11,8 +11,17 @@ CF = np.array([100,100,1100,0,0])
 p = krd.pvalcf(T,CF,Y)
 keybond = krd.krdbond(T,CF,Y)
 
+def pr(effY,n):
+    yr = ((1+effY)**(1./n) - 1)
+    return yr
 
+ym = pr(.046,12)
+    
+T = np.array([1./12,2./12,3./12,4./12,5./12,6./12])
+Y = np.array([pr(.046,12),pr(.046,6),pr(.046,4),pr(.046,3),pr(.046,5./12),pr(.046,2)])
+CF = np.array([0,70,0,70,0,1070])
 
+keybond = krd.krdbond(T,CF,Y)
 
 # [2] Portfolio construction and key rate duration
 #      Based on Example 9.1 of Nawalkha et al. (2005)
