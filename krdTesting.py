2d8a3a4c25dc088b49c2cd764876b29df0c260b2
angusman <agusman@csu.fullerton.edu>
2016-11-14 04:52:35 -0800

    revisions for monthly computation
    based on derivations discussed in appendix of our paper

--- krdTesting.py
+++ krdTesting.py
@@ -1,39 +1,79 @@
 import krdur as krd
 import numpy as np
 
+# NOTES:
+#  .All computations performed assuming daily compounding (n = 365)
+#  .All vectors and array elements are assumed to be encoded by the month
+#    i.e., an interest rate vector Y = [.05,.06,.07] will be interpreted as
+#    the 1-month, 2-month, and 3-month interest rates
+#  .KRD compuatations require 'annualized' effective rates in current testing
+#  .KRD computations require monthly term vector to be converted in terms of
+#    years; e.g., T = [1,2] months    must be inputted as 
+#    T = (1./12)*[1,2] years; this is likely a consequence of using annualized
+#    interest rates
+#  .Portfolio computations currently performed on class object BONDS
+#  .Cash flow objects are Numpy arrays produced by class krd.cBond()
+
+
+# Coupon bond class object
+cb = krd.cBond()
 
 
 # [1] Single bond present value and key rate durations
-T = np.array([1,2,3,4,5])
-Y = 1./100 * np.array([5,5.5,5.75,5.9,6])
-CF = np.array([100,100,1100,0,0])
+T0 = (1./12)*np.arange(1,60+1,1)
+Y0 = np.array([0,0,0,0,0,0,0,0,0,0,0,.05,
+               0,0,0,0,0,0,0,0,0,0,0,.055,
+               0,0,0,0,0,0,0,0,0,0,0,.0575,
+               0,0,0,0,0,0,0,0,0,0,0,.059,
+               0,0,0,0,0,0,0,0,0,0,0,.06])
+CF0 = cb.new(1000,12,1,.10);  CF0 = cb.shift(CF0,1,60)
+p = krd.pvalcf(T0,CF0,Y0)
+keybond0 = krd.krdbond(T0,CF0,Y0)
 
-p = krd.pvalcf(T,CF,Y)
-keybond = krd.krdbond(T,CF,Y)
 
-def pr(effY,n):
-    yr = ((1+effY)**(1./n) - 1)
-    return yr
 
-ym = pr(.046,12)
+# [2] Test Case by Nathan
+def exxP(effY,n):
+    # !! KRD computation requires annualized effective rates.
+    py = n*((1+effY)**(1./n) - 1)
+    return py
+
+ym = exxP(.046,365)   # sample annualized exact rate
     
-T = np.array([1./12,2./12,3./12,4./12,5./12,6./12])
-Y = np.array([pr(.046,12),pr(.046,6),pr(.046,4),pr(.046,3),pr(.046,5./12),pr(.046,2)])
-CF = np.array([0,70,0,70,0,1070])
+T6month = (1./12)*np.array([1,2,3,4,5,6])
+Y6month = ym*np.ones((1,6))
+CF6month = cb.new(1000,6,6,.07)
+keybond6month = krd.krdbond(T6month,CF6month,Y6month)
 
+
+
+# [3] Example from textbook
+T = (1./12)*np.arange(1,60+1,1)
+Y = np.array([0,0,0,0,0,0,0,0,0,0,0,.05,
+              0,0,0,0,0,0,0,0,0,0,0,.055,
+              0,0,0,0,0,0,0,0,0,0,0,.0575,
+              0,0,0,0,0,0,0,0,0,0,0,.059,
+              0,0,0,0,0,0,0,0,0,0,0,.06])
+CF = cb.new(1000,60,1,0.1)
 keybond = krd.krdbond(T,CF,Y)
 
-# [2] Portfolio construction and key rate duration
+
+
+# [4] Portfolio construction and key rate duration
 #      Based on Example 9.1 of Nawalkha et al. (2005)
-Bond1 = krd.cBond(1000,1,1,.10,fillTill=5)
-Bond2 = krd.cBond(1000,2,1,.10,fillTill=5)
-Bond3 = krd.cBond(1000,3,1,.10,fillTill=5)
-Bond4 = krd.cBond(1000,4,1,.10,fillTill=5)
-Bond5 = krd.cBond(1000,5,1,.10,fillTill=5)
+Bond1 = cb.new(1000,12,1,.10);  Bond1 = cb.shift(Bond1,1,60)
+Bond2 = cb.new(1000,24,1,.10);  Bond2 = cb.shift(Bond2,1,60)
+Bond3 = cb.new(1000,36,1,.10);  Bond3 = cb.shift(Bond3,1,60)
+Bond4 = cb.new(1000,48,1,.10);  Bond4 = cb.shift(Bond4,1,60)
+Bond5 = cb.new(1000,60,1,.10);  Bond5 = cb.shift(Bond5,1,60)
     
 class BONDS:
-    T = np.array([1,2,3,4,5])
-    Y = 1./100 * np.array([5,5.5,5.75,5.9,6])
+    T = (1./12)*np.arange(1,60+1,1)
+    Y = np.array([0,0,0,0,0,0,0,0,0,0,0,.05,
+                  0,0,0,0,0,0,0,0,0,0,0,.055,
+                  0,0,0,0,0,0,0,0,0,0,0,.0575,
+                  0,0,0,0,0,0,0,0,0,0,0,.059,
+                  0,0,0,0,0,0,0,0,0,0,0,.06])
     CF = np.array([Bond1,Bond2,Bond3,Bond4,Bond5])       
 
 W = np.array([0.2,0.2,0.2,0.2,0.2])
--- krdur.py
+++ krdur.py
@@ -15,24 +15,32 @@ import numpy as np                               # MATLAB-style functions
 
 
 
-def krdbond(T,CF,Y):
-#   Computes key rate durations of cash flows. Assuming continuous compounding.
+def krdbond(T,CF,Y,mode='disc'):
+#   Computes key rate durations of cash flows.
 #    T    Term structure;  vector 1-by-N
 #    CF   Cash flow;       vector 1-by-N
 #    Y    Yield structure; vector 1-by-N    
-    P = pvalcf(T,CF,Y)
-    KRD = 1/P * CF*T/np.exp(Y*T)
+    P = pvalcf(T,CF,Y,mode)
+    if mode=='cont':
+        KRD = 1/P * CF*T/np.exp(Y*T)
+    elif mode=='disc':
+        n = 365
+        KRD = 1/P * CF*T/((1+Y/n)**(n*T+1))
     return KRD
 
     
      
-def pvalcf(T,CF,Y,mode='cont'):
-#   Computes present value of cash flows. Assuming continuous compounding.
+def pvalcf(T,CF,Y,mode='disc'):
+#   Computes present value of cash flows.
 #    T    Term structure;  vector 1-by-N
 #    CF   Cash flow;       vector 1-by-N
 #    Y    Yield structure; vector 1-by-N
     if mode=='cont':
         s = CF/np.exp(Y*T)
+        P = np.sum(s)
+    elif mode=='disc':
+        n = 365
+        s = CF/((1+Y/n)**(n*T))
         P = np.sum(s)
     return P
 #######################################1#######################################
@@ -59,33 +67,64 @@ def krdport(W,BONDS):
     return KRDport
     
     
+'''
+# Now defunct procedure. Originally intended to produce an arbitrary timing
+#  vector T. Abandoned due to difficulties aligning with monthly step sizes.
     
-def cBond(faceValue,matYears,annualCouponNum,couponRate,offset=0,fillTill=None,):
-#   Constructs coupon bond.
+def cBond(faceValue,matMonths,annualCouponNum,couponRate):
+#   Constructs coupon bond. Assuming equally spaced payments.
 #    faceValue         Face value of bond;  scalar
-#    matYears          Years to maturity;   scalar
+#    matMonths         Months to maturity;   scalar
 #    annualCouponNum   Number of coupons per year;   scalar
 #    couponRate        Coupon value as a percent of face value;   scalar
-#
-#    OPTIONAL: These are usually used to align bonds for portfolios.
-#    offset            periods to prefill with zeros
-#    fillTill          periods to postfill with zeros
-    L = np.round(matYears*annualCouponNum)
-    if fillTill==None:
-        L0 = L
-    else:
-        L0 = np.round(fillTill)
+    n = int(np.round(matMonths*(1./12)*annualCouponNum))   # number of payments
+    s = matMonths*(1./n)                                            # step size
     cc = couponRate * faceValue
-    C = np.zeros((1,L0))
-    for i in np.arange(offset,offset+L):
-        C[0,i] = cc
-        if i==L-1:
-            C[0,i] += faceValue
-    return C
+    C = np.zeros((1,n));  T = np.zeros((1,n))
+    for i in range(n):
+        C[0,i] = cc;  T[0,i] = s*(i+1)
+        if i==n-1:
+            C[0,i] += faceValue 
+    return C, T
+'''
 #######################################2#######################################
+class cBond:
+    def new(self,faceValue,matMonths,annualCouponNum,couponRate):
+    #   Constructs coupon bond at monthly scale. Assuming equally spaced 
+    #    payments. Also assuming annualCouponNum is a factor of 12 and that 
+    #    matMonths is a multiple of this factor.
+    #
+    #    faceValue         Face value of bond;  scalar
+    #    matMonths         Months to maturity;   scalar
+    #    annualCouponNum   Number of coupons per year;   scalar
+    #    couponRate        Coupon value as a percent of face value;   scalar
+        if 12 % annualCouponNum != 0:
+            raise RuntimeError('annualCouponNum must be a factor of 12')    
+        k = 12/annualCouponNum
+        if matMonths % k != 0:
+            raise RuntimeError('matMonths must align with annualCouponNum')        
+        cc = couponRate * faceValue
+        C = np.zeros((1,matMonths))
+        for i in np.arange(k-1,matMonths,k):
+            C[0,i] = cc
+            if i==matMonths-1:
+                C[0,i] += faceValue 
+        return C
+    
 
-
-
+    def shift(self,bond,startMonth,endMonth):
+    #   Shifts coupon bond for alignment with other bonds and timing.
+    #
+    #    bond          Bond object constructed by .new function
+    #    startMonth    Month bond timing begins (prepending 0s)
+    #    endMonth      Month bond tracking ends (postpending 0s)
+        prepend = np.zeros((1,startMonth-1))
+        sbond = np.append(prepend,bond)
+        if endMonth > bond.size:
+            postpend = np.zeros((1,endMonth-bond.size))
+            sbond = np.append(sbond,postpend)
+        return sbond
+#######################################3#######################################
 
 
 
