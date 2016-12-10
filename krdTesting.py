5280582cd9ea4ddecd5fc548301d39cac8974014
angusman <agusman@csu.fullerton.edu>
2016-12-10 03:04:28 -0800

    overwriting old krdTesting script

--- krdTesting.py
+++ krdTesting.py
@@ -1,213 +1,189 @@
-import numpy as np
-import pandas as pd
-import datetime
-from dateutil.relativedelta import relativedelta
-import krdur as krd
-import portfoliofuns as pf
+# KEY RATE DURATION TUTORIAL
+#    Calculate key rate durations of individual bonds and/or portfolios.
+#
+# 8 December 2016
+# Anthony Gusman, Nathan Johnson, Nick Sullivan
+#
+# References:
+#    Nawalkha, Soto, and Beliaeva. 2005. Interest Rate Risk Modeling: The 
+#       Fixed Income Valuation Course. John Wiley & Sons, Inc. New Jersey.
 
 
-# TODO: Solve mismatched case
+
+import numpy as np                               # MATLAB-style functions
+import scipy as sp
+import pandas as pd                              # Dataframe functions
+from scipy import optimize
+from datetime import timedelta
 
 
-# Coupon bond class object
-cbond = pf.cBond()
+import krdur as krd                              # Key rate duration
+import my_immunization as im                     
 
 
-# [1] Single 1-coupon bond present value and key rate durations
-dt_str = '1/1/2012'
-createDate = datetime.datetime.strptime(dt_str, "%m/%d/%Y").date()
-CF0, T0 = cbond.new(100,12,4,.10,createDate)
-Y0 = np.array([.06])
-bond = { "interestrate" : Y0,
-         "createdate" : createDate,
-         "coupondates" : T0,
-         "couponpayments" : CF0} 
-keybond0 = krd.krdbond(bond)
+
+
+
+#%% [0] Loading and preparing interest rate data...
+startdate = '3/1/2006'
+enddate = '2/1/2016'
+Data = pd.read_csv("trimmed_data.csv")     # treasury maturity rates over time
+
+
+# Preprocessing...
+Data = Data.as_matrix()                       # ...removes row IDs and headers
+Data = Data[:, 2:11]                      # cut 'Unnamed' and 'daynumber' cols
+
+
+# Create 'I': Filtered dataframe by business days; restricted to date interval                                        
+Data = pd.DataFrame(Data[:,1:], index = Data[:,0], columns = ['1.0', '3.0', '6.0',
+                    '12.0', '24.0', '36.0', '60.0', '84.0'])
+dates = pd.date_range(startdate, enddate, freq = 'BMS')  # business day filter
+date_strs = pd.Series(dates.format())
+I = Data.ix[date_strs]
+
+
+# Cleaning data...
+for x in range(dates.size):
+    rate1 = I.ix[date_strs.ix[x]].ix[0];
+    if np.isnan(rate1):
+        new_date =  dates[x] + timedelta(1)
+        new_index = pd.date_range(new_date, new_date)
+        new_string = pd.Series(new_index.format())[0]
+
+        # Special cases
+        if (new_string == '2010-01-02'):
+            new_string = '2010-01-04'
+        if (new_string == '2016-01-02'):
+            new_string = '2016-01-04'
+
+        I.ix[date_strs.ix[x]] = Data.ix[new_string]    # replace with new data
+        I.T.columns.values[x] = new_string             # rename accordingly
+
+
+# Converting: annual percent >> annual decimal >> monthly decimal  
+I = I/100
+M = im.my_monthly_effective_rate(I)
+
+
+
+#%% [1] Creating a portfolio and extracting a bond...
+
+# Setting global maturity lengths (Types) and coupons per year (CPY)...
+possibleTypes = np.array([1.0, 3.0, 6.0, 12.0, 24.0, 36.0, 60.0, 84.0])
+possibleCPY = np.array([1, 3, 4, 6, 12])     
+
+
+# Building an asset portfolio...            
+maxMonths = 84                                       # longest maturity length
+numBonds = np.random.randint(20, 51)            # number of bonds in portfolio
+PORT = im.my_portfolio_generator(numBonds, maxMonths)
+[PORT_assets, PORT_types, PORT_CPYs] = [PORT[0], PORT[1], PORT[2]]
+
+
+# Extracing a particular bond...
+bondID = 0;
+bond_CF   = PORT_assets[bondID]
+bond_type = PORT_types[bondID]
+bond_CPY  = PORT_CPYs[bondID]
+
+
+
+
+# 
+L_portfolio = np.zeros((numBonds,maxMonths))
+L_number = np.zeros(numBonds)
+L_duration = np.zeros(numBonds)
+
+
+
+#%% [2] Interpolating interest rates for key rate duration calculations...
+windowSize = 36
+S = M.ix[windowSize-1]                             # current schedule
+U = [float(S.index[i]) for i in range(S.size)]   # extract terms
+V = [float(S.values[i]) for i in range(S.size)]  # extract rates
+X = np.arange(maxMonths)+1                         # interpolated terms
+Y = np.interp(X,U,V)                            # interpolated rates
+
+
+
+
+
+
+
+#%% [3] Finding present value and key rate duration of bond...
+K = np.array([1, 3, 6, 12, 24, 36, 60, 84])                    # set key rates
+bond_KRD,bond_PV = krd.bond(bond_CF,Y,K)
+
+print "Key rate durations (KRD):"      ;  print bond_KRD
+print "Sum of krd: "                   ;  print np.sum(bond_KRD)
+print "Present value (KRD): "          ;  print bond_PV
+
+
+# Difference compared to standard valuation...
+asset_rate = im.my_extract_rates(M, bond_type)
+asset_rate = asset_rate[windowSize-1] 
+pval = im.my_present_value(bond_CF,asset_rate)
+macD = im.my_macD(bond_CF,asset_rate)
   
-print "KRD of 0-coupon bond";  print np.sum(keybond0[0]); 
-print "Present value: ";  print np.sum(keybond0[1]);  print ' '
-
-
-#%%
-
-# [2] Test Case by Nathan
-dt_str = '1/1/2016'
-createDate = datetime.datetime.strptime(dt_str, "%m/%d/%Y").date()
-CF6month, T6month = cbond.new(1000,6,6,.07,createDate)
-Y6month = np.array([.046])
-bond = { "interestrate" : Y6month,
-         "createdate" : createDate,
-         "coupondates" : T6month,
-         "couponpayments" : CF6month} 
-keybond6month = krd.krdbond(bond)
-
-print "KRD of 6-month bond (2 month durations & full duration)"
-print keybond6month[0]; print np.sum(keybond6month[0]); 
-print "Present value: ";  print keybond6month[1];  print ' '
-
-newtime = createDate + relativedelta(months=5)
-keybond6monthnew = krd.krdbond(bond,currentTime=newtime)
-
-print "KRD of 6-month bond (later date)"
-print keybond6monthnew[0]; print np.sum(keybond6monthnew[0]); 
-print "Present value: ";  print keybond6monthnew[1];  print ' '
-
-
-#%%
-
-# [3] Example from textbook
-dt_str = '1/1/2012'
-createDate = datetime.datetime.strptime(dt_str, "%m/%d/%Y").date()
-CF, T = cbond.new(1000,5*12,1,.10,createDate)
-T = [createDate + relativedelta(years=i+1) for i in range(5)]
-Y = np.array([.05,.055,.0575,.059,.06])
-bond = { "interestrate" : Y,
-         "createdate" : createDate,
-         "coupondates" : T,
-         "couponpayments" : CF} 
-keybond = krd.krdbond(bond,mode='cont')
-
-print "KRD of 5-year bond (1 year durations & full duration)"
-print keybond[0];  print np.sum(keybond[0]);
-print "Present value: ";  print keybond[1];  print ' '
-
-
-#%%
-
-# [4] Portfolio construction and key rate duration
-#      Based on Example 9.1 of Nawalkha et al. (2005)
-dt_str = '1/1/2012'
-createDate = datetime.datetime.strptime(dt_str, "%m/%d/%Y").date() 
-   
-bondConstructor = {
-    "faceValues" : 1000*np.ones(5),
-    "matMonths" : 12*np.arange(1,5+1,1),
-    "annualCouponNum" : [1 for i in range(5)],
-    "couponRate" : .10*np.ones(5),
-    "createDate" : [createDate for i in range(5)],
-    "interestrate" : [np.array([.05,.055,.0575,.059,.06]) for i in range(5)],
-    "weight" : (1./5) * np.ones(5)                 }
+print "Macaulay duration (my): "       ;  print macD
+print "Modified duration (my): "       ;  print macD/(1+asset_rate)
+print "Present value (my): "           ;  print pval               ;  print ' '
 
 
 
-def bonder(bC):
-    # Creates a portfolio of bonds as specified by bond constructor bC.
-    b = int(bC["weight"].size)
-    L = ["interestrate","weight","createdate","coupondates","couponpayments"] 
-    A = [ [bC["createDate"][i] for j in range(b-i-1)] for i in range(b) ]
-    D = [cbond.new(bC["faceValues"][i],   # coupon dates
-                    bC["matMonths"][i],
-                    bC["annualCouponNum"][i],
-                    bC["couponRate"][i],
-                    bC["createDate"][i])[1] for i in range(b)]
-    V = [bC["interestrate"],
-         bC["weight"],
-         [bC["createDate"][i] for i in range(b)],
-         [ D[i] + A[i] for i in range(b)],
-         [np.append(cbond.new(bC["faceValues"][i],   # coupon payments
-                    bC["matMonths"][i],
-                    bC["annualCouponNum"][i],
-                    bC["couponRate"][i],
-                    bC["createDate"][i])[0],np.zeros(b-i-1)) for i in range(b)]
-          ]           
-    portfolio = [{L[j]: V[j][i] for j in range(5)} for i in range(b) ]
-    return portfolio
+#%% [4] Finding present value and key rate duration of portfolio...
+Q = np.ones(numBonds)
+PORT_KRD,PORT_PV = krd.portfolio(PORT_assets,Y,Q,K)
 
-       
-portfolio = bonder(bondConstructor)
-portfolio_df = pd.DataFrame(portfolio)    
 
-#newtime = pf.date_to_day(createDate + relativedelta(months=15))
-newtime = createDate
-keyport = krd.krdport(portfolio_df,newtime,mode='disc')
 
-print "KRD of portfolio with 1-5 year bonds (1 year durations & full duration)"
-print keyport[0];  print np.sum(keyport[0]); 
-print "Present value: ";  print keyport[1]; print np.sum(keyport[1]);  print ' '
 
-#%%
+#%% [5] Creating liability types for KRD solution...
+NL = K.size+1;                             # one more than key rate durations
+maturities = np.array([1,3,6,12,24,36,60,84,1,3,6,12,24,36,60,84])
+coupon_rate = np.array([1, 3, 4, 6, 12])
 
-# [5] Instantaneous Returns
-#     Shocking the yield curve
-Yold = np.array([.05,.055,.0575,.059,.06])
-Ynew = np.array([.055,.057,.0575,.058,.058])
 
-def iret(T,CF,Yold,Ynew,mode):
-    # Instantaneous return dP/P
-    R = (pf.pval(T,CF,Ynew,mode) - pf.pval(T,CF,Yold,mode))/pf.pval(T,CF,Yold,mode)
-    return R
+def Lgen(N,max_months,possible_duration,coupon_rate):
+    Coupons_per_year = np.zeros(N)
+    Interest = np.zeros(N)
+    Pl = np.zeros((N,max_months))
+    r = np.arange(N)
+    Typel = np.zeros(N)
     
-T = np.array([[1, 0, 0, 0, 0],
-              [1, 2, 0, 0, 0],
-              [1, 2, 3, 0, 0],
-              [1, 2, 3, 4, 0],
-              [1, 2, 3, 4, 5]])
-CF = np.array([[1100, 0, 0, 0, 0],
-               [100, 1100, 0, 0, 0],
-               [100, 100, 1100, 0, 0],
-               [100, 100, 100, 1100, 0],
-               [100, 100, 100, 100, 1100]])
-
-
-
-def myinstantrun(T,CF,Yold,Ynew,mode):
-    R = [];  D = np.append(T,CF,1)
-    for d in D:
-        R = np.append(R,iret(d[0:5],d[5:],Yold,Ynew,mode))
-    return R
+    for x in r:
+        i = np.random.uniform(0.01, 0.1)
+        Interest[x] = i
+        possible_duration = possible_duration[possible_duration <= max_months]
+        tipe = possible_duration[x]
+        Typel[x] = tipe
+        cr = coupon_rate[coupon_rate <= tipe]
+        ncp = cr[np.random.randint(np.size(cr))] # calculate the number of
+                                                 # coupon payments in a year
+        Coupons_per_year[x] = ncp
+        Pl[x] = im.my_bond_generator(max_months, tipe, 1, i, ncp)
     
     
-R = myinstantrun(T,CF,Yold,Ynew,'cont')
-for r in R:
-    print "Bond: {0:.3f}%".format(100*r)
-    
-    
-# Weighting the bonds invested in...
-ladder = np.array([0.2, 0.2, 0.2, 0.2, 0.2])
-barbell = np.array([0.479, 0, 0, 0, 0.521])
-bullet = np.array([0, 0.521, 0, 0.479, 0])
+        L_portfolio = Pl
+        L_Type = Typel
+        L_CPY = Coupons_per_year
+    return L_portfolio, L_Type, L_CPY
 
-portfolio_ladderR = np.sum(ladder*R)
-print "Ladder: {0:.3f}%".format(100*portfolio_ladderR)
-
-portfolio_barbellR = np.sum(barbell*R)
-print "Barbell: {0:.3f}%".format(100*portfolio_barbellR)
-
-portfolio_bulletR = np.sum(bullet*R)
-print "Bullet: {0:.3f}%".format(100*portfolio_bulletR)
+[LPORT_CF, LPORT_types, LPORT_CPYs] = Lgen(NL,maxMonths,maturities,coupon_rate)
 
 
-#%%
 
-# [6] Key Rate Immunization (based on [4])
-bond1 = portfolio[0];    bond2 = portfolio[1];    bond3 = portfolio[2]
-bond4 = portfolio[3];    bond5 = portfolio[4];
-bond6 = { "interestrate" : np.array([0.05, 0.055, 0.0575, 0.059, 0.06]),
-         "createdate" : createDate,
-         "coupondates" : [createDate, createDate, 
-                          createDate, createDate,
-                          createDate+relativedelta(years=5)],
-         "couponpayments" : np.array([0, 0, 0, 0, 1000])} 
+#%% [6] Finding immunized position...
 
-KRD1 = krd.krdbond(bond1,mode='cont')[0]
-KRD2 = krd.krdbond(bond2,mode='cont')[0]
-KRD3 = krd.krdbond(bond3,mode='cont')[0]
-KRD4 = krd.krdbond(bond4,mode='cont')[0]
-KRD5 = krd.krdbond(bond5,mode='cont')[0]
-KRD6 = krd.krdbond(bond6,mode='cont')[0]
+# immunize(Pa,Y,Qa,Pl,K,w0='null',r=0.5):
+Qa = np.ones(krd.nbonds(PORT_assets))
+N2short,err,w = krd.immunize(PORT_assets,Y,Qa,LPORT_CF,K)
 
-# Form KRD matrix
-K = []
-K = np.append(K,[KRD1, KRD2, KRD3, KRD4, KRD5, KRD6])
-K = K.reshape(6,len(K)/6);  K = K.T
-K = np.concatenate((K,np.array([[1, 1, 1, 1, 1, 1]])),axis=0)
+print 'Recommended short amounts: ';   print np.negative(N2short)
 
-h = 4                                                # planning horizon (years)
-DUR = np.array([0,0,0,4,0,1])
-DUR = DUR.reshape(6,1)
 
-# Solve for proportions of investments
-# VERY BAD CONDITION NUMBER! Requires pseudo-inverse
-# Unstable solution! Depends on day calculation, compounding, days per year...
-p = np.linalg.lstsq(K,DUR)
+
+
+
 
