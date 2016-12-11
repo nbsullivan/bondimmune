5af65bbb9dbb3ffe92bf30d7982713d8c3a102fd
angusman <agusman@csu.fullerton.edu>
2016-12-11 05:31:10 -0800

    possible fix for krd immunization nan

--- bond_project_funs.py
+++ bond_project_funs.py
@@ -133,10 +133,6 @@ def otherMonth(max_months,Portfolio_A,Portfolio_L,N,I,considered,Vasicek,
         newPortfolio_A = np.delete(Portfolio_A, np.arange(x-1), axis = 1)
         newPortfolio_L = np.delete(Portfolio_L, np.arange(x-1), axis = 1)
         Portfolio_L_krd = Portfolio_L[0:NL]
-
-        new_Liability_number_krd = np.zeros(NL)
-        new_VLiability_number = np.zeros(N)
-        new_VLiability_number_krd = np.zeros(NL)
         
         transaction = np.zeros(N)
         vtransaction = np.zeros(N)
@@ -147,28 +143,16 @@ def otherMonth(max_months,Portfolio_A,Portfolio_L,N,I,considered,Vasicek,
         ExpectedChange = np.zeros(N)
         VExpectedChange = np.zeros(N)
        
+        
+        
         # Find the maximum expected change in price based on data as well as
         # Vasicek estimate
-        ExpectedChange, VExpectedChange = expectedChange(N,Type,considered,Coupons_per_year,monthly_rates,
-                   newPortfolio_A,Vasicek,x,ExpectedChange,VExpectedChange)
-        '''
-        for y in np.arange(N):
-            asset_rate = im.my_extract_rates(monthly_rates, Type[y])
-            old_asset_rate = asset_rate[considered-2]
-            asset_rate = asset_rate[considered-1]
-            diff = np.absolute(old_asset_rate - asset_rate)
-            
-            vasset_rate = im.my_extract_rates(Vasicek,Type[y])
-            old_vasset_rate = vasset_rate[x-1]
-            vasset_rate = vasset_rate[x]
-            vdiff = np.absolute(old_vasset_rate - vasset_rate)
-            
-            ExpectedChange[y] = diff*im.my_Price_Change(newPortfolio_A[y],asset_rate, Coupons_per_year[y])
-            VExpectedChange[y] = vdiff*im.my_Price_Change(newPortfolio_A[y], vasset_rate, Coupons_per_year[y])
-        #FOR expected end 
-        '''
+        ExpectedChange, VExpectedChange = expectedChange(N,Type,considered,
+                   Coupons_per_year,monthly_rates,newPortfolio_A,Vasicek,x,
+                   ExpectedChange,VExpectedChange)
         maxChange = np.max(ExpectedChange)
         vmaxChange = np.max(VExpectedChange)
+        
         
         
         # Calculate transaciton costs based on data
@@ -177,6 +161,7 @@ def otherMonth(max_months,Portfolio_A,Portfolio_L,N,I,considered,Vasicek,
                     monthly_rates,newPortfolio_A,newPortfolio_L,
                     transaction_cost,Liability_number,x,'L')    
         Transaction[x] = np.sum(transaction)
+        
         
         
         # Skip index for krd
@@ -194,43 +179,19 @@ def otherMonth(max_months,Portfolio_A,Portfolio_L,N,I,considered,Vasicek,
         use = use[index==1]
         vuse = vuse[vindex==1]
         
+
         
         # KRD computation (data)
-        Qa = np.ones(krd.nbonds(Portfolio_A[use]))
-        interp_rates = krd.rateinterp(monthly_rates,considered,max_months)   # krd
-        N2short,err,w = krd.immunize(Portfolio_A[use],interp_rates,Qa,Portfolio_L_krd,K,r=1)
-        if np.isnan(N2short).any():
-            print('NaN detected! Optimizer failed to converge! Switching to Vasiceck model...')
-            interp_rates = krd.rateinterp(Vasicek,considered-37,max_months)   # krd
-            Qa = np.ones(krd.nbonds(Portfolio_A[vuse]))
-            N2short,err,w = krd.immunize(Portfolio_A[vuse],interp_rates,Qa,Portfolio_L_krd,K,r=1)
-            
-            new_Liability_number_krd = np.negative(N2short)
-    
-            transaction_krd = np.zeros(transaction.shape)
-            acquisition_krd = np.zeros(transaction.shape)
-            Lkrd = np.zeros(transaction.shape)
-            for y in np.arange(NL):
-                acquisition_krd[y] = np.abs(new_Liability_number_krd[y]-Liability_number_krd[y])*krd.bond(Portfolio_L_krd[y],interp_rates,K)[1]
-                Lkrd[y] = new_Liability_number_krd[y]*krd.bond(Portfolio_L_krd[y],interp_rates,K)[1]
-            net_krd = np.abs(krd.portfolio(Portfolio_A[vuse],interp_rates,Qa,K)[1] - np.sum(Lkrd)) 
-        
-            transaction_krd = transaction_cost*(net_krd + np.sum(acquisition_krd))
-        else:    
-            new_Liability_number_krd = np.negative(N2short)
-    
-            transaction_krd = np.zeros(transaction.shape)
-            acquisition_krd = np.zeros(transaction.shape)
-            Lkrd = np.zeros(transaction.shape)
-            for y in np.arange(NL):
-                acquisition_krd[y] = np.abs(new_Liability_number_krd[y]-Liability_number_krd[y])*krd.bond(Portfolio_L_krd[y],interp_rates,K)[1]
-                Lkrd[y] = new_Liability_number_krd[y]*krd.bond(Portfolio_L_krd[y],interp_rates,K)[1]
-            net_krd = np.abs(krd.portfolio(Portfolio_A[use],interp_rates,Qa,K)[1] - np.sum(Lkrd)) 
-        
-            transaction_krd = transaction_cost*(net_krd + np.sum(acquisition_krd))
-        
-        Liability_number_krd = new_Liability_number_krd
-        
+        if use.shape[0] != 0:    # some bonds need to be immunized
+            Qa = np.ones(krd.nbonds(Portfolio_A[use]))
+            interp_rates = krd.rateinterp(monthly_rates,considered,max_months)   # not the cause of nan
+            N2short,err,w = krd.immunize(Portfolio_A[use],interp_rates,Qa,Portfolio_L_krd,K,r=1)
+            transaction_krd, Liability_number_krd = transactionCostKRD(N2short,
+                       transaction,NL,Liability_number_krd,Portfolio_L_krd,
+                       interp_rates,K,Portfolio_A,use,Qa,transaction_cost)
+        else:
+            print('KRD (data): No immunization required...')
+            transaction_krd = 0         # and leave Liability_number_krd alone 
         Transaction_krd[x] = np.sum(transaction_krd)
         
         
@@ -244,26 +205,21 @@ def otherMonth(max_months,Portfolio_A,Portfolio_L,N,I,considered,Vasicek,
         VTransaction[x] = np.sum(vtransaction)  
         
 
-        # KRD computation
-        interp_rates = krd.rateinterp(Vasicek,considered-37,max_months)   # krd
-        Qa = np.ones(krd.nbonds(Portfolio_A[vuse]))
-        N2short,err,w = krd.immunize(Portfolio_A[vuse],interp_rates,Qa,Portfolio_L_krd,K,r=1)
-        new_VLiability_number_krd = np.negative(N2short)
-    
-        vtransaction_krd = np.zeros(vtransaction.shape)
-        acquisition_krd = np.zeros(vtransaction.shape)
-        Lkrd = np.zeros(vtransaction.shape)
-        #vtransaction_krd[NL] = krd.portfolio(Portfolio_A[NL:],interp_rates,Qa[NL:],K)[1]
-        for y in np.arange(NL):
-            acquisition_krd[y] = np.abs(new_VLiability_number_krd[y]-VLiability_number_krd[y])*krd.bond(Portfolio_L_krd[y],interp_rates,K)[1]
-            Lkrd[y] = new_VLiability_number_krd[y]*krd.bond(Portfolio_L_krd[y],interp_rates,K)[1]
-        net_krd = np.abs(krd.portfolio(Portfolio_A[vuse],interp_rates,Qa,K)[1] - np.sum(Lkrd)) 
         
-        vtransaction_krd = transaction_cost*(net_krd + np.sum(acquisition_krd))
-        VLiability_number_krd = new_VLiability_number_krd
-        
+        # KRD computation (Vasicek)
+        if vuse.shape[0] != 0:    # some bonds need to be immunized
+            Qa = np.ones(krd.nbonds(Portfolio_A[vuse]))        
+            interp_rates = krd.rateinterp(Vasicek,considered-37,max_months)   # krd
+            N2short,err,w = krd.immunize(Portfolio_A[vuse],interp_rates,Qa,Portfolio_L_krd,K,r=1)        
+            vtransaction_krd, VLiability_number_krd = transactionCostKRD(N2short,
+                           vtransaction,NL,VLiability_number_krd,Portfolio_L_krd,
+                           interp_rates,K,Portfolio_A,vuse,Qa,transaction_cost)
+        else:
+            print('KRD (Vasicek): No immunization required...')
+            vtransaction_krd = 0       # and leave VLiability_number_krd alone
         VTransaction_krd[x] = np.sum(vtransaction_krd)
 
+        
         
         considered = considered + 1
     #FOR x all other months end        
@@ -338,3 +294,25 @@ def expectedChange(N,Type,considered,Coupons_per_year,monthly_rates,
         ExpectedChange[y] = diff*im.my_Price_Change(newPortfolio_A[y],asset_rate, Coupons_per_year[y])
         VExpectedChange[y] = vdiff*im.my_Price_Change(newPortfolio_A[y], vasset_rate, Coupons_per_year[y])
     return ExpectedChange, VExpectedChange
+    
+    
+    
+    
+    
+def transactionCostKRD(N2short,transaction,NL,Liability_number_krd,
+                       Portfolio_L_krd,interp_rates,K,Portfolio_A,use,Qa,
+                       transaction_cost):
+    new_Liability_number_krd = np.negative(N2short)
+    
+    transaction_krd = np.zeros(transaction.shape)
+    acquisition_krd = np.zeros(transaction.shape)
+    Lkrd = np.zeros(transaction.shape)
+    for y in np.arange(NL):
+        acquisition_krd[y] = np.abs(new_Liability_number_krd[y]-Liability_number_krd[y])*krd.bond(Portfolio_L_krd[y],interp_rates,K)[1]
+        Lkrd[y] = new_Liability_number_krd[y]*krd.bond(Portfolio_L_krd[y],interp_rates,K)[1]
+    net_krd = np.abs(krd.portfolio(Portfolio_A[use],interp_rates,Qa,K)[1] - np.sum(Lkrd)) 
+        
+    transaction_krd = transaction_cost*(net_krd + np.sum(acquisition_krd))
+    Liability_number_krd = new_Liability_number_krd
+    return transaction_krd, Liability_number_krd
+    
